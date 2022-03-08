a16837f6f762d1edc4e9ad484de2f489694e9c05
Polymeth <rayan.boucheny@epfl.ch>
2022-03-08 18:34:31 +0100

    almost finished sampledProfiles+fixed on GraphEdges+ratio zumba cafew

--- src/ch/epfl/javelo/data/GraphEdges.java
+++ src/ch/epfl/javelo/data/GraphEdges.java
@@ -1,9 +1,11 @@
 package ch.epfl.javelo.data;
 
 import ch.epfl.javelo.Bits;
+import ch.epfl.javelo.Math2;
 import ch.epfl.javelo.Q28_4;
 
 import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
 
@@ -13,6 +15,12 @@ public record GraphEdges(ByteBuffer edgesBuffer, IntBuffer profileIds, ShortBuff
     //   8  |  8  |  8  |  8  |  8  |  8  |  8  |  8  |   8  |   8  |
     // inv+target             | longueur  |  denivlé  | osm shit
 
+    private static final int EMPTY = 0;
+    private static final int UNCOMPRESSED = 1;
+    private static final int COMPRESSED_Q_4_4 = 2;
+    private static final int COMPRESSED_Q_0_4 = 3;
+
+    //todo: correct index
     public boolean isInverted(int edgeId) {
         return Bits.extractUnsigned(edgesBuffer.getInt(edgeId*2), 31, 1) == 1;
     }
@@ -42,7 +50,51 @@ public record GraphEdges(ByteBuffer edgesBuffer, IntBuffer profileIds, ShortBuff
         return Bits.extractUnsigned(profileIds.get(0), 30, 2) != 0;
     }
 
+    // 16 | 4 4 4 4 | 4 4 4 4 | 4
+
     public float[] profileSamples(int edgeId) {
-        
+        // taille du tableau = nbEchantillon
+        int firstIndex = Bits.extractUnsigned(profileIds.get(edgeId), 0, 30);
+        int samplesNumber = 1 + Math2.ceilDiv(Short.toUnsignedInt(edgesBuffer.getShort(4 + edgeId * 10)), Q28_4.ofInt(2)); // number of samples
+        int count = 0;
+
+        switch (Bits.extractUnsigned(profileIds.get(edgeId), 30, 2)) {
+            case EMPTY:
+                return new float[0];
+            case UNCOMPRESSED:
+               /* for (int i = 0; i < samplesNumber; i++) {
+                    int lol = elevations.get(firstIndex + i);
+                    System.out.println(lol);
+                }*/
+                break;
+            case COMPRESSED_Q_4_4:
+                break;
+
+            case COMPRESSED_Q_0_4:
+                float[] decompressed = new float[samplesNumber];
+                for (int i = 0; i < Math2.ceilDiv(samplesNumber-1, 4)+1; i++) {
+                    if (i == 0) {
+                        decompressed[i] = Q28_4.asFloat(elevations.get(firstIndex));
+                    } else {
+                        for (int j = 0; j < 4; j++) {
+                            float difference = Q28_4.asFloat(Bits.extractSigned(elevations.get(firstIndex + i), 12-4*j, 4));
+                            decompressed[i+j+3*(i-1)] = (decompressed[i+j+3*(i-1)-1])+difference;
+                            //System.out.println(decompressed[i+j+3*(i-1)] + " & " + difference);
+                        }
+                    }
+                }
+               /** for (int i = 0; i < samplesNumber; i++) {
+                    if (i == 0) {
+                        yolo[i] = Q28_4.asFloat(elevations.get(firstIndex));
+                    } else { //todo: horrible boucle
+                        for (int j = 0; j < 3; j++) {
+                            yolo[] = Q28_4.asFloat(Bits.extractSigned(elevations.get(firstIndex+i), 4*j, 4));
+                        }
+                    }
+
+                }*/
+                return decompressed;
+        }
+        return new float[0];
     }
 }

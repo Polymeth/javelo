3c64fe545698214e2e1805fe8dba4db2d745d29d
HighPointFR <38163674+HighPointFR@users.noreply.github.com>
2022-03-10 18:37:11 +0100

    GraphSectors Done + tested

--- src/ch/epfl/javelo/data/GraphSectors.java
+++ src/ch/epfl/javelo/data/GraphSectors.java
@@ -1,5 +1,6 @@
 package ch.epfl.javelo.data;
 
+import ch.epfl.javelo.Math2;
 import ch.epfl.javelo.projection.PointCh;
 
 import java.nio.ByteBuffer;
@@ -26,7 +27,7 @@ public record GraphSectors(ByteBuffer buffer) {
      *
      * @param center Coordinates of center of radius (in PointCh format)
      * @param distance distance in meters from center
-     * @return ArrayList of Sectors that have a
+     * @return ArrayList of Sectors that are in the radius of the center
      */
     public List<Sector> sectorsInArea(PointCh center, double distance){
         ArrayList<Sector> sectors = new ArrayList<Sector>();
@@ -36,53 +37,38 @@ public record GraphSectors(ByteBuffer buffer) {
         double center_e = center.e(); //centre du parametre
         double center_n = center.n();
 
-        double down_square_e = center_e - distance;
-        double down_square_n = center_n - distance;
+        double down_square_e = (center_e - distance);
+        double down_square_n = (center_n - distance);
 
-        double up_square_e = center_e + distance;
-        double up_square_n = center_n + distance;
+        double up_square_e = (center_e + distance);
+        double up_square_n = (center_n + distance);
 
-        int downsector_e = (int)((down_square_e -MIN_E)/((2.7265625)*1000)); //index du secteur en bas a gauche
-        if(downsector_e < 0){
-            downsector_e = 0;
-        }else if (downsector_e > 127){
-            downsector_e = 127;
-        }
-        int downsector_n = (int)((down_square_n -MIN_N)/((2.7265625)*1000)); //index du secteur en bas a gauche
-        if(downsector_n < 0){
-            downsector_n = 0;
-        }else if (downsector_n > 127){
-            downsector_n = 127;
-        }
+        //index du secteur en bas a gauche
+        int downsector_e = (int)((down_square_e -MIN_E)/((2.7265625)*1000));
+        downsector_e = Math2.clamp(0, downsector_e, 127);
 
-        int upsector_e = (int)(( up_square_e - MAX_E)/((2.7265625)*1000));//index du secteur en haut a droite //Todo check les bounds avec MAXE et MINE
-        if(upsector_e < 0){
-            upsector_e = 0;
-        }else if (upsector_e > 127){
-            upsector_e = 127;
-        }
+        int downsector_n = (int)((down_square_n -MIN_N)/((1.7265625)*1000));
+        downsector_n = Math2.clamp(0, downsector_n, 127);
 
-        int upsector_n = (int)(( up_square_n- MAX_N)/((2.7265625)*1000));
-        if(upsector_n < 0){
-            upsector_n = 0;
-        }else if (upsector_n > 127){
-            upsector_n = 127;
-        }
+        //index du secteur en haut a droite
+        int upsector_e = (int)(( up_square_e - MIN_E)/((2.7265625)*1000))+1;
+        upsector_e = Math2.clamp(0, upsector_e, 128);
+
+        int upsector_n = (int)(( up_square_n- MIN_N)/((1.7265625)*1000))+1;
+        upsector_n = Math2.clamp(0, upsector_n, 128);
 
 
+        for(int j = downsector_n; j < upsector_n; j ++){
+            for(int i = downsector_e; i < upsector_e; i++){
+            int index = 128* j + i;
+            int bytes_index = index * SECTOR_BYTES;
 
+            int firstnode = buffer.getInt(bytes_index);
+            int lastnodes = firstnode + Short.toUnsignedInt(buffer.getShort(bytes_index + OFFSET_BYTES)); //id first node + last 2 bytes of buffer (aka number of nodes)
 
-        for(int i = downsector_e; i < upsector_e; i++){
-            for(int j = downsector_n; j < upsector_n; j ++){
-                int index = 128* j + i;
-                int bytes_index = index * SECTOR_BYTES;
-                int firstnode = buffer.getInt(bytes_index); //todo verifier unsigned
-                System.out.println("Firstnode = " + firstnode);
-                int lastnodes = firstnode + Short.toUnsignedInt(buffer.getShort(bytes_index + OFFSET_BYTES)); //id first node + last 2 bytes of buffer (aka number of nodes)
-                System.out.println("Lastnode = " + lastnodes);
-                Sector sect = new Sector(firstnode, lastnodes);
-                sectors.add(sect);
-            }
+            Sector sect = new Sector(firstnode, lastnodes);
+            sectors.add(sect);
+             }
         }
 
         return sectors;

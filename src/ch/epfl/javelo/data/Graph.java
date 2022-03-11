b84f132c828abacee38434005a77484e6a987904
HighPointFR <38163674+HighPointFR@users.noreply.github.com>
2022-03-11 16:17:56 +0100

    Beggining step 4 elevation done to be tested

--- src/ch/epfl/javelo/data/Graph.java
+++ src/ch/epfl/javelo/data/Graph.java
@@ -1,68 +1,152 @@
 package ch.epfl.javelo.data;
 
+import ch.epfl.javelo.Functions;
 import ch.epfl.javelo.projection.PointCh;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 
 public final class Graph {
-    public Graph(GraphNodes nodes, GraphSectors sectors, GraphEdges edges, List<AttributeSet> attributeSets){
 
+    private GraphNodes nodes;
+    private GraphSectors sectors;
+    private GraphEdges edges;
+    private List<AttributeSet> attributeSets;
+
+    public Graph(GraphNodes nodes, GraphSectors sectors, GraphEdges edges, List<AttributeSet> attributeSets){
+        this.nodes = nodes;
+        this.sectors = sectors;
+        this.edges = edges;
+        this.attributeSets = List.copyOf(attributeSets);
     }
 
+    /**
+     * @param basePath path of the folder where the .bin files are stored
+     * @return a JaVelo graph made from the files data
+     * @throws IOException
+     */
     public static Graph loadFrom(Path basePath) throws IOException {
-        Path filePath = Path.of("lausanne/nodes_osmid.bin");
-        LongBuffer osmIdBuffer;
-        try (FileChannel channel = FileChannel.open(filePath)) {
-            osmIdBuffer = channel
+        Path nodesPath = Path.of("nodes.bin");
+
+        // todo: ne pas mettre dans une liste?
+        Path[] paths = {
+                Path.of("nodes.bin"),
+                Path.of("profile_ids.bin"),
+                Path.of("sectors.bin"),
+                Path.of("edges.bin"),
+                Path.of("elevations.bin"),
+                Path.of("attributes.bin")
+        };
+
+        IntBuffer nodesBuffer, profileIdsBuffer;
+        ByteBuffer sectorsBuffer, edgesBuffer;
+        ShortBuffer elevationsBuffer;
+        LongBuffer attributesBuffer;
+
+        // todo faire une fonction avec le try with ressources
+        try (FileChannel channel = FileChannel.open(basePath.resolve(paths[0]))) {
+            nodesBuffer = channel
+                    .map(FileChannel.MapMode.READ_ONLY, 0, channel.size())
+                    .asIntBuffer();
+        }
+        try (FileChannel channel = FileChannel.open(basePath.resolve(paths[1]))) {
+            profileIdsBuffer = channel
+                    .map(FileChannel.MapMode.READ_ONLY, 0, channel.size())
+                    .asIntBuffer();
+        }
+        try (FileChannel channel = FileChannel.open(basePath.resolve(paths[2]))) {
+            sectorsBuffer = channel
+                    .map(FileChannel.MapMode.READ_ONLY, 0, channel.size())
+                    .asReadOnlyBuffer();
+        }
+        try (FileChannel channel = FileChannel.open(basePath.resolve(paths[3]))) {
+            edgesBuffer = channel
+                    .map(FileChannel.MapMode.READ_ONLY, 0, channel.size())
+                    .asReadOnlyBuffer();
+        }
+        try (FileChannel channel = FileChannel.open(basePath.resolve(paths[4]))) {
+            elevationsBuffer = channel
+                    .map(FileChannel.MapMode.READ_ONLY, 0, channel.size())
+                    .asShortBuffer();
+        }
+        try (FileChannel channel = FileChannel.open(basePath.resolve(paths[5]))) {
+            attributesBuffer = channel
                     .map(FileChannel.MapMode.READ_ONLY, 0, channel.size())
                     .asLongBuffer();
         }
-        System.out.println(osmIdBuffer.get(2022));
-        return null;
+
+        GraphNodes graphNodes = new GraphNodes(nodesBuffer);
+        GraphSectors graphSectors = new GraphSectors(sectorsBuffer);
+        GraphEdges graphEdges = new GraphEdges(edgesBuffer, profileIdsBuffer, elevationsBuffer);
+        ArrayList<AttributeSet> attributeSet = new ArrayList<>();
+
+        for (int i = 0; i < attributesBuffer.capacity(); i++) {
+            AttributeSet yolo = new AttributeSet(attributesBuffer.get(i));
+            attributeSet.add(yolo);
+        }
+
+        return new Graph(graphNodes, graphSectors, graphEdges, attributeSet);
+    }
+
+    public PointCh nodePoint(int nodeId){
+        return new PointCh(nodes.nodeE(nodeId), nodes.nodeN(nodeId));
     }
 
     public int nodeCount(){
-        return 0;
+        return nodes.count();
     }
 
     public int nodeOutDegree(int nodeId){
-        return 0;
+        return nodes.outDegree(nodeId);
     }
 
     public int nodeOutEdgeId(int nodeId, int edgeIndex){
-        return 0;
+        return nodes.edgeId(nodeId, edgeIndex);
     }
 
     public int nodeClosestTo(PointCh point, double searchDistance){
+        List<GraphSectors.Sector> sectorsWithinPoint = sectors.sectorsInArea(point, searchDistance);
+        int lowestNode = sectorsWithinPoint.get(0).startNodeId();
+        int highestNode = sectorsWithinPoint
         return 0;
     }
 
     public int edgeTargetNodeId(int edgeId){
-        return 0;
+        return edges.targetNodeId(edgeId);
     }
 
     public boolean edgeIsInverted(int edgeId){
-        return false;
+        return edges.isInverted(edgeId);
     }
 
     public AttributeSet edgeAttributes(int edgeId){
-        return null;
+        return attributeSets.get(edges.attributesIndex(edgeId));
     }
 
     public double edgeLength(int edgeId){
-        return 0;
+        return edges.length(edgeId);
     }
 
     public double edgeElevationGain(int edgeId){
-        return 0;
+        return edges.elevationGain(edgeId);
     }
 
     public DoubleUnaryOperator edgeProfile(int edgeId){
-        return null;
+        if (!edges.hasProfile(edgeId)) {
+            return (x) -> Double.NaN;
+        } else {
+            return Functions.sampled(edges.profileSamples(edgeId), edges.length(edgeId));
+        }
     }
+
 }
--- src/ch/epfl/javelo/routing/Edge.java
+++ src/ch/epfl/javelo/routing/Edge.java
@@ -1,17 +1,35 @@
 package ch.epfl.javelo.routing;
 
+import ch.epfl.javelo.Math2;
 import ch.epfl.javelo.data.Graph;
 import ch.epfl.javelo.projection.PointCh;
 
 import java.util.function.DoubleUnaryOperator;
 
 public record Edge(int fromNodeId, int toNodeId, PointCh fromPoint, PointCh toPoint, double length, DoubleUnaryOperator profile) {
+
     public static Edge of(Graph graph, int edgeId, int fromNodeId, int toNodeId) {
-        return null;
+
+        PointCh frompoint1 = graph.nodePoint(fromNodeId);
+        PointCh topoint1 = graph.nodePoint(toNodeId);
+        double edgelength = graph.edgeLength(edgeId);
+        DoubleUnaryOperator profile = graph.edgeProfile(edgeId);
+
+        Edge edge = new Edge(fromNodeId, toNodeId, frompoint1, topoint1, edgelength, profile);
+
+        return edge;
     }
 
     public double positionClosestTo(PointCh point) {
-        return 0;
+        double ax = fromPoint.e()
+        double ay = fromPoint.n()
+        double bx = toPoint.e()
+        double by = toPoint.n()
+        double px = point.e()
+        double py = point.n()
+
+
+        return Math2.projectionLength(ax, ay, bx, by, px, py);
     }
 
     public PointCh pointAt(double position) {

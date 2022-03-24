82ee7801028d1caef2b3e8217765353eee0678bf
Polymeth <rayan.boucheny@epfl.ch>
2022-03-24 16:45:25 +0100

    fixed SingleRoute out of bounds problems TODO: make the code joli

--- src/ch/epfl/javelo/routing/SingleRoute.java
+++ src/ch/epfl/javelo/routing/SingleRoute.java
@@ -8,13 +8,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * @author Rayan BOUCHENY (327575)
+ * @author Loris Tran (341214)
+ */
 public final class SingleRoute implements Route {
     private final ArrayList<Edge> allEdges = new ArrayList<>();
+    private final double[] distances;
 
     public SingleRoute(List<Edge> edges) {
-       Preconditions.checkArgument(edges.size() != 0);
+        Preconditions.checkArgument(edges.size() != 0);
         for (int i = 0; i < edges.size(); i++) {
             allEdges.add(edges.get(i));
+        }
+
+        this.distances = new double[allEdges.size()+1];
+        distances[0] = 0;
+
+        for (int i = 1; i < distances.length; i++) {
+            distances[i] = distances[i - 1] + (allEdges.get(i-1).length());
         }
     }
 
@@ -39,49 +51,63 @@ public final class SingleRoute implements Route {
 
     @Override
     public List<PointCh> points() {
-        double[] distances = distanceList();
         List<PointCh> points = new ArrayList<>();
-        for (double distance : distances) {
-            points.add(pointAt(distance));
+        points.add(pointAt(0));
+        for (int i = 1; i < distances.length; i++) {
+            points.add(pointAt(distances[i]));
         }
         return points;
     }
 
     @Override
     public PointCh pointAt(double position) {
-        double[] distances = distanceList();
         int edgeId = edgeIndexAtPosition(position);
         return (edgeId < distances.length-1)
                 ? allEdges.get(edgeId).pointAt(position - distances[edgeId])
                 : allEdges.get(edgeId-1).pointAt(allEdges.get(edgeId-1).length()); // kinda sus
-        // todo passer le bidule du edgeId - 1 dans la methode index
     }
 
     @Override
     public int nodeClosestTo(double position) {
-        double[] distances = distanceList();
-        int edgeId = edgeIndexAtPosition(position);
-        double distanceOnEdge = position - distances[edgeId];
+        position = correctedPosition(position);
+        int index = Arrays.binarySearch(distances, position);
 
-        if (distanceOnEdge < allEdges.get(edgeId).length()) {
-            //SEX
+        if (index >= 0) {
+            return (index<allEdges.size()) ? allEdges.get(index).fromNodeId() : allEdges.get(index-1).toNodeId();
+        } else {
+            index = -(index + 2);
+            double normFirstNode = Math2.norm(position - allEdges.get(index).fromPoint().e(), position - allEdges.get(index).fromPoint().n());
+            double normSecondNode = Math2.norm(position - allEdges.get(index).toPoint().e(), position - allEdges.get(index).toPoint().n());
+            return (normFirstNode < normSecondNode) ? allEdges.get(index).fromNodeId() : allEdges.get(index).toNodeId();
         }
-
-        return 0;
-
-
     }
 
     @Override
     public RoutePoint pointClosestTo(PointCh point) {
         RoutePoint minimum = RoutePoint.NONE;
-        //for (Edge edge : allEdges) {
-
-        //}
-        return null;
+        for (Edge edge : allEdges) {
+            double distance = Math2.clamp(0, edge.positionClosestTo(point), edge.length());
+            minimum = minimum.min(edge.pointAt(distance), distance, edge.pointAt(distance).distanceTo(point));
+        }
+        return minimum;
     }
 
     @Override
+    public double elevationAt(double position) {
+        position = correctedPosition(position);
+        int index = Arrays.binarySearch(distances, position);
+
+        if (index < 0) {
+            index = -(index)-2;
+            return (allEdges.get(index).elevationAt(position - distances[index]));
+        } else if (index >= allEdges.size()) {
+            return (allEdges.get(index-1).elevationAt(position - distances[index-1]));
+        } else {
+            return (allEdges.get(index).elevationAt(position - distances[index]));
+        }
+    }
+
+    /*@Override
     public double elevationAt(double position) {
         //double[] distances = {0d, 5800d, 8100d, 9200d, 11400d, 13100d};
         double[] distances = distanceList();
@@ -92,27 +118,18 @@ public final class SingleRoute implements Route {
             return allEdges.get(edgeId-1).elevationAt(allEdges.get(edgeId-1).length());
         }
         return allEdges.get(edgeId).elevationAt(position - distances[edgeId]);
-    }
+    }*/
 
     private int edgeIndexAtPosition(double position) {
-        double[] distances = distanceList();
         return (Arrays.binarySearch(distances, position) < 0) ? -(Arrays.binarySearch(distances, position) + 2) : Arrays.binarySearch(distances, position);
     }
 
-    /**
-     * @return a
-     */
-    private double[] distanceList() {
-        double[] distances = new double[allEdges.size()+1];
-
-        for (int i = 0; i <= allEdges.size(); i++) {
-            if (i != 0) {
-                Edge edge = allEdges.get(i-1);
-                distances[i] = distances[i-1] + allEdges.get(i-1).length();
-            } else {
-                distances[i] = 0d;
-            }
+    private double correctedPosition(double position) {
+        if (position < 0) {
+            return 0d;
+        } else if (position > length()) {
+            return length();
         }
-        return distances;
+        return position;
     }
 }

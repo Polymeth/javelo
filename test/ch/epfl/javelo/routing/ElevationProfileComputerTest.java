71b832573af7f6d60fba2961d5479a557b8a95a1
HighPointFR <38163674+HighPointFR@users.noreply.github.com>
2022-03-24 16:51:03 +0100

    ElevationProfile + Single route

--- test/ch/epfl/javelo/routing/ElevationProfileComputerTest.java
+++ test/ch/epfl/javelo/routing/ElevationProfileComputerTest.java
@@ -264,12 +264,12 @@ class ElevationProfileComputerTest {
 
         DoubleUnaryOperator profile = Functions.sampled(elevationSamples, 10);
 
-        Edge edge1 = new Edge(0, 3, fromPoint, toPoint, 10, profile);
+        Edge edge1 = new Edge(0, 3, fromPoint, toPoint, 5, profile);
         ArrayList<Edge> testEdges = new ArrayList<Edge>();
         testEdges.add(edge1);
         SingleRoute route = new SingleRoute(testEdges);
 
-        double maxStepLength = 1.8;
+        double maxStepLength = 1.5;
         int stepNumber = (int)Math.ceil(route.length() / maxStepLength) +1;
         double distanceBetweenPoints = route.length() / stepNumber;
 
@@ -294,21 +294,21 @@ class ElevationProfileComputerTest {
 
         // profile for edge 1
         float[] type3Array = new float[]{(float) Double.NaN, (float) Double.NaN,  0f, (float) Double.NaN, (float) Double.NaN, 3f, (float) Double.NaN, 5f, (float) Double.NaN};
-        DoubleUnaryOperator profile = Functions.sampled(type3Array, 10);
+        DoubleUnaryOperator profile = Functions.sampled(type3Array, 9);
 
         // profile for edge 2
         float[] type3Array2 = new float[]{(float) Double.NaN, (float) Double.NaN,  0f, (float) Double.NaN, (float) Double.NaN, 3f, (float) Double.NaN, 5f, (float) Double.NaN};
-        DoubleUnaryOperator profile2 = Functions.sampled(type3Array, 10);
+        DoubleUnaryOperator profile2 = Functions.sampled(type3Array, 9);
 
-        Edge edge1 = new Edge(0, 3, fromPoint, toPoint, 10, profile);
-        Edge edge2 = new Edge(0, 3, fromPoint, toPoint, 10, profile2);
+        Edge edge1 = new Edge(0, 3, fromPoint, toPoint, 9, profile);
+        Edge edge2 = new Edge(0, 3, fromPoint, toPoint, 9, profile2);
 
         ArrayList<Edge> testEdges = new ArrayList<Edge>();
         testEdges.add(edge1);
         testEdges.add(edge2);
         SingleRoute route = new SingleRoute(testEdges);
 
-        double maxStepLength = 1;
+        double maxStepLength = 1.2;
         int stepNumber = (int)Math.ceil(route.length() / maxStepLength) +1;
         double distanceBetweenPoints = route.length() / stepNumber;
 

51a64f5efd5cc428e01adf3c045db86d3d6fa084
Polymeth <rayan.boucheny@epfl.ch>
2022-04-26 09:47:37 +0200

    added WaypointsManager

--- src/ch/epfl/javelo/gui/BaseMapManager.java
+++ src/ch/epfl/javelo/gui/BaseMapManager.java
@@ -1,31 +1,32 @@
 package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.Math2;
-import ch.epfl.javelo.projection.PointCh;
-import ch.epfl.javelo.projection.PointWebMercator;
-import ch.epfl.javelo.projection.WebMercator;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ObjectPropertyBase;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.collections.ObservableList;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.canvas.*;
-
-import java.awt.*;
 import java.io.IOException;
 
 public final class BaseMapManager {
     private final TileManager tileManager;
+    private final WaypointsManager waypointsManager;
     private final ObjectProperty<MapViewParameters> property;
     private boolean redrawNeeded;
 
     private final Pane pane;
     private final Canvas canvas;
 
-    public BaseMapManager(TileManager tileManager, ObjectProperty<MapViewParameters> property) {
+    public BaseMapManager(TileManager tileManager, WaypointsManager waypointsManager, ObjectProperty<MapViewParameters> property) {
         this.tileManager = tileManager;
         this.property = property;
         this.redrawNeeded = true;
+        this.waypointsManager = waypointsManager;
 
         pane = new Pane();
         canvas = new Canvas();
@@ -34,20 +35,52 @@ public final class BaseMapManager {
         canvas.widthProperty().bind(pane.widthProperty());
         canvas.heightProperty().bind(pane.heightProperty());
 
-        canvas.widthProperty().addListener(o -> System.out.printf("New canvas width: %.2f\n", canvas.getWidth()));
+        SimpleLongProperty minScrollTime = new SimpleLongProperty();
+        pane.setOnScroll(e -> {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime < minScrollTime.get()) return;
+            minScrollTime.set(currentTime + 250);
+            double zoomDelta = Math.signum(e.getDeltaY());
+            int oldZoom = property.get().zoomlevel();
+            int newZoom = (int)Math2.clamp(8, this.property.get().zoomlevel() + zoomDelta, 19);
+            System.out.println(newZoom);
+            double multiplier = (oldZoom < newZoom) ? 2 : 0.5;
+            this.property.set(new MapViewParameters(newZoom,
+                    property.get().originXcoord()*multiplier,
+                    property.get().originYcoord()*multiplier));
+            redrawOnNextPulse();
+        });
+
         canvas.sceneProperty().addListener((p, oldS, newS) -> {
             assert oldS == null;
             newS.addPreLayoutPulseListener(this::redrawIfNeeded);
         });
 
-        System.out.println("canvas size: " + canvas.getWidth());
         redrawOnNextPulse();
     }
 
     private void redrawIfNeeded() {
         if (!redrawNeeded) return;
         //redrawNeeded = false;
-        pane();
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        try {
+            Point2D topLeft = property.get().topLeft();
+            Point2D bottomRight = new Point2D(property.get().topLeft().getX() + canvas.getWidth(),
+                    property.get().topLeft().getY() + canvas.getHeight());
+
+            for(int y = (int)topLeft.getY()/256; y <= (int)bottomRight.getY()/256; y++) {
+                for(int x = (int)topLeft.getX()/256; x <= (int)bottomRight.getX()/256; x++) {
+                    System.out.println("zoom " + property.get().zoomlevel() + ", x: " + x + ", y: " + y);
+                    TileManager.TileId toDraw = new TileManager.TileId(property.get().zoomlevel(), x, y);
+
+                    int imageX = x*256 - (int)topLeft.getX();
+                    int imageY = y*256 - (int)topLeft.getY();
+                    context.drawImage(tileManager.imageForTileAt(toDraw), imageX, imageY);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private void redrawOnNextPulse() {
@@ -56,38 +89,8 @@ public final class BaseMapManager {
     }
 
     public Pane pane() {
-        GraphicsContext context = canvas.getGraphicsContext2D();
-        try {
-            Point2D topLeft = property.get().topLeft();
-            Point2D bottomLeft = new Point2D(property.get().topLeft().getX(),
-                    property.get().topLeft().getY() + canvas.getHeight());
-            Point2D topRight = new Point2D(property.get().topLeft().getX() + canvas.getWidth(),
-                    property.get().topLeft().getY());
-            Point2D bottomRight = new Point2D(property.get().topLeft().getX() + canvas.getWidth(),
-                    property.get().topLeft().getY() + canvas.getHeight());
-
-            for(int y = (int)topLeft.getY()/256; y <= (int)bottomLeft.getY()/256; y++) {
-                for(int x = (int)topLeft.getX()/256; x <= (int)topRight.getX()/256; x++) {
-                   // System.out.println(x + "; " + y);
-                    TileManager.TileId toDraw = new TileManager.TileId(12, x, y);
-                    int imageX = x*256 - (int)topLeft.getX();
-                    int imageY = y*256 - (int)topLeft.getY();
-                    context.drawImage(tileManager.imageForTileAt(toDraw), imageX, imageY);
-                }
-            }
-          //  System.out.println("------");
-
-
-
-           // System.out.println(topLeft.getX() + " " + topLeft.getY());
-
-            TileManager.TileId toDraw = new TileManager.TileId(12, 0, 0);
-            //context.drawImage(tileManager.imageForTileAt(toDraw), 0, 0);
-
-
-        } catch (IOException e) {
-            System.out.println("error");
-        }
+        waypointsManager.pane();
         return this.pane;
     }
+
 }

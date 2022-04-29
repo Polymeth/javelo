ca4e8289e0e1794e1d66782922f6c0f1b7a3b6ae
HighPointFR <38163674+HighPointFR@users.noreply.github.com>
2022-04-29 17:24:55 +0200

    WayPointsManager FInished But gotta clean the code

--- src/ch/epfl/javelo/gui/WaypointsManager.java
+++ src/ch/epfl/javelo/gui/WaypointsManager.java
@@ -71,25 +71,6 @@ public final class WaypointsManager {
         return pane;
     }
 
-    private Group drawPin(double x, double y) {
-        SVGPath outsidePoint = new SVGPath();
-        outsidePoint.setContent("M-8-20C-5-14-2-7 0 0 2-7 5-14 8-20 20-40-20-40-8-20");
-        outsidePoint.getStyleClass().add("pin_outside");
-
-        SVGPath insidePoint = new SVGPath();
-        insidePoint.setContent("M0-23A1 1 0 000-29 1 1 0 000-23");
-        insidePoint.getStyleClass().add("pin_inside");
-
-        Group group = new Group();
-        group.getStyleClass().add("pin");
-        group.getChildren().add(outsidePoint);
-        group.getChildren().add(insidePoint);
-
-        group.setLayoutX(property.get().originXcoord() + x);
-        group.setLayoutY(property.get().originYcoord() + y);
-
-        return group;
-    }
 
     private Group createPin(int i){
         //todo ptet modulariser on verra
@@ -115,12 +96,7 @@ public final class WaypointsManager {
         group.setLayoutX(property.get().viewX(pointMercator));
         group.setLayoutY(property.get().viewY(pointMercator));
 
-        group.setOnMouseReleased(e -> {
-            if (e.isStillSincePress()) {
-                waypoints.remove(waypoints.get(i));
-                System.out.println("test1");
-            }
-        });
+
 
         ObjectProperty<Point2D> pointPressed = new SimpleObjectProperty<>();
         ObjectProperty<Boolean> isBeingDragged = new SimpleObjectProperty<>();
@@ -133,14 +109,38 @@ public final class WaypointsManager {
         group.setOnMouseDragged(e -> {
             double x = group.getLayoutX();
             double y = group.getLayoutY();
-            //group.setLayoutX();
-            drawPin(e.getX(), e.getY());
+            group.setLayoutX(e.getX() + x - pointPressed.get().getX());
+            group.setLayoutY(e.getY() + y - pointPressed.get().getY());
+
         });
 
         group.setOnMouseReleased(e -> {
+            if (e.isStillSincePress()) {
+                waypoints.remove(waypoints.get(i));
+                System.out.println("deleting point");
+            } else {
+                //PointWebMercator newPoint = PointWebMercator.of(property.get().zoomlevel(), property.get().originXcoord() + e.getX(), property.get().originYcoord() + e.getY());
+               // System.out.println("new point is x: " + mousePoint + "; " + finalY);
+                //
+                Waypoint newPoint = placeWaypoint(group.getLayoutX(),  group.getLayoutY());
 
+                //System.out.println("sont affichés être en " + property.get().viewX(newPoint) + " ;" + property.get().viewY(newPoint));
+
+                //PointCh pointchtest = newPoint.toPointCh();
+                //int nodeId = graph.nodeClosestTo(newPoint.point(), 500); //todo fix node id
+
+                waypoints.set(i, newPoint);
+                createWaypoints();
+
+            }
+
+            // Waypoint[point=PointCh[e=2535651.836318822, n=1155128.3486538585], nodeId=107825]
         });
 
+        /*group.setOnMouseReleased(e -> {
+            double finalX = property.get().originXcoord() + e.getX();
+            double finalY = property.get().originYcoord() + e.getY();
+            PointWebMercator newPoint = PointWebMercator.of(property.get().zoomlevel(), finalX, finalY);
 
         return group;
     }
@@ -148,7 +148,7 @@ public final class WaypointsManager {
     private void createWaypoints() {
         pane.getChildren().clear();
         for(int i = 0; i < waypoints.size(); i++){
-
+            System.out.println("ntm");
             Group group = createPin(i);
 
             if(i != 0 && i != waypoints.size() -1){
@@ -167,10 +167,23 @@ public final class WaypointsManager {
         }
     }
 
+    public Waypoint placeWaypoint(double x, double y){
+        PointWebMercator pointWBM = PointWebMercator.of(property.get().zoomlevel(), x, y);
+        PointCh pos = property.get().pointAt(x, y).toPointCh();
+        System.out.println(" waypoint");
+        int nodeid = graph.nodeClosestTo(pos, 500);
+        if (nodeid == -1){
+            error.accept("Aucune route à proximité !");
+            return null;
+        } else {
+            return new Waypoint(pos, nodeid);
+        }
+    }
+
     public void addWaypoint(double x, double y) {
         //todo utiliser x at zoom level ? ou view x
         PointWebMercator pointWBM = PointWebMercator.of(property.get().zoomlevel(), x, y);
-
+        System.out.println("adding waypoint");
         int nodeid = graph.nodeClosestTo(pointWBM.toPointCh(), 500);
         if (nodeid == -1){
             error.accept("Aucune route à proximité !");
@@ -178,6 +191,10 @@ public final class WaypointsManager {
             waypoints.add(new Waypoint(pointWBM.toPointCh(), nodeid));
         }
 
+    }
+
+    public ObjectProperty<Boolean> isBeingDragged(){
+        return this.isBeingDragged;
     }
 
 }

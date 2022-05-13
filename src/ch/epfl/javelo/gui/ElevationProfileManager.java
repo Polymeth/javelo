93866249448acad454648e4b168a6bb5e62a71a9
Polymeth <rayan.boucheny@epfl.ch>
2022-05-13 15:05:23 +0200

    Started drawing elevations

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -1,9 +1,13 @@
 package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.routing.ElevationProfile;
-import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -12,16 +16,23 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.NonInvertibleTransformException;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ElevationProfileManager {
     private final ReadOnlyObjectProperty<ElevationProfile> profile;
     private final ReadOnlyDoubleProperty mousePositionOnProfile;
 
     private final BorderPane borderPane;
+    private final ObjectProperty<Rectangle2D> profileRectangle = new SimpleObjectProperty<>();
+    private final Polygon graph;
+    private final Pane pane;
 
-    public ElevationProfileManager(ReadOnlyObjectProperty<ElevationProfile> profile, ReadOnlyDoubleProperty mousePositionOnProfile) {
+    public ElevationProfileManager(ReadOnlyObjectProperty<ElevationProfile> profile, ReadOnlyDoubleProperty mousePositionOnProfile) throws NonInvertibleTransformException {
         this.profile = profile;
         this.mousePositionOnProfile = mousePositionOnProfile;
 
@@ -37,14 +48,17 @@ public final class ElevationProfileManager {
                 profile.get().length(),
                 profile.get().totalAscent(),
                 profile.get().totalDescent(),
-                profile.get().maxElevation(),
-                profile.get().minElevation()));
+                profile.get().minElevation(),
+                profile.get().maxElevation()));
 
         VBox vbox = new VBox(statText);
         vbox.setId("profile_data");
         borderPane.setBottom(vbox);
 
         // center
+        pane = new Pane();
+        Insets insets = new Insets(10, 40, 20, 10);
+
         Text test1 = new Text("0m");
         Text test2 = new Text("100m");
 
@@ -57,16 +71,70 @@ public final class ElevationProfileManager {
         Path path = new Path();
         path.setId("grid");
 
-        Polygon graph = new Polygon();
+        graph = new Polygon();
         graph.setId("profile");
 
         Line highlightedPoint = new Line();
-
-        Pane pane = new Pane();
         pane.getChildren().addAll(path, gridTexGroup, highlightedPoint, graph);
-        Insets insets = new Insets(10, 10, 20, 40);
-        borderPane.setCenter(pane);
 
+        // profile drawing
+        profileRectangle.set(Rectangle2D.EMPTY);
+
+        profileRectangle.bind(Bindings.createObjectBinding(() -> {
+            double width = pane.getWidth() - insets.left - insets.right;
+            double height = pane.getHeight() - insets.top - insets.bottom;
+
+            if (width < 0) width = 0;
+            if (height < 0) height = 0;
+            System.out.println("lol");
+            return new Rectangle2D(40, 10, width, height);
+        }, pane.widthProperty(), pane.heightProperty()));
+
+        profileRectangle.addListener(e -> {
+            try {
+                drawElevations();
+            } catch (NonInvertibleTransformException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        borderPane.setCenter(pane);
+    }
+
+    private Affine affineScreenToWorld(Rectangle2D rect){
+        Affine affine = new Affine();
+
+        affine.prependTranslation(-rect.getMinX(), -rect.getMinY());
+        affine.prependScale( profile.get().length() / (rect.getMaxX() - rect.getMinX()),
+                (profile.get().minElevation()-profile.get().maxElevation()) / (rect.getMaxY() - rect.getMinY()));
+        affine.prependTranslation(0, profile.get().maxElevation());
+        return affine;
+    }
+
+    private void drawElevations() throws NonInvertibleTransformException {
+        graph.getPoints().clear();
+        List<Double> pointsToAdd = new ArrayList<>();
+
+        Affine screenToWorld = affineScreenToWorld(profileRectangle.get());
+        Affine worldToScreen = screenToWorld.createInverse();
+
+        for (int x = (int)profileRectangle.get().getMinX(); x < profileRectangle.get().getMaxX(); x++) {
+            Point2D height = screenToWorld.transform(x, 0);
+
+            Point2D transform = worldToScreen.transform(x, profile.get().elevationAt(height.getX()));
+            pointsToAdd.add((double)x);
+            pointsToAdd.add(transform.getY());
+
+            System.out.println(x + ", " + transform.getY());
+        }
+
+        pointsToAdd.add(profileRectangle.get().getMaxX());
+        pointsToAdd.add(profileRectangle.get().getMaxY());
+
+        pointsToAdd.add(profileRectangle.get().getMinX());
+        pointsToAdd.add(profileRectangle.get().getMaxY());
+
+        graph.getPoints().addAll(pointsToAdd);
     }
 
 
